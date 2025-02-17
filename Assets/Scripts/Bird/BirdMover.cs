ce1c34a16859f2badbbdd7ee07d11c8226168686
8eastwood <m.fishborn@yandex.ru>
2025-02-17 19:47:52 +0300

    flappy is born

---
+++ Assets/Scripts/Bird/BirdMover.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BirdMover : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _maxRotationZ;
+    [SerializeField] private float _minRotationZ;
+    [SerializeField] private float _tapForce;
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _rigidbody;
+    private Quaternion _minRotation;
+    private Quaternion _maxRotation;
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _maxRotation = Quaternion.Euler(0f, 0f, _maxRotationZ);
+        _minRotation = Quaternion.Euler(0f, 0f, _minRotationZ);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.velocity = new Vector2(_speed, _tapForce);
+            transform.rotation = _maxRotation;
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, _minRotation, _rotationSpeed * Time.deltaTime);
+    }
+}
